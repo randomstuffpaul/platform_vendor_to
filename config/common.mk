4f8286815fc8be7c884cf370a6db5054600cd2cb
hedwig34 <hedwig34@gmail.com>
2015-12-11 14:44:19 -0500

    Add SuperSU (1 of 2)
      SuperSU v2.78
    
    Change-Id: I2bbcc1afa8f5b69947081f1ccc3879a37e8086f0

--- config/common.mk
+++ config/common.mk
@@ -51,6 +51,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/to/prebuilt/common/supersu/supersu.zip:supersu/supersu.zip
+
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=1
 
