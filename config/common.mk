35090e7e1f80dc2b0487e0b8b25958969eb96500
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-07-06 15:18:29 -0700

    Adapt to 6.0.1_r52 backup whitelist
    As of f/b 9b8c6d2df35455ce9e67907edded1e4a2ecb9e28
     we need to whitelist who can be backup services.
    
    Change-Id: I52a004924e6d2651c5ecdec51a8c5a80bdd54a38

--- config/common.mk
+++ config/common.mk
@@ -40,6 +40,10 @@ PRODUCT_COPY_FILES += \
     vendor/to/prebuilt/common/bin/50-cm.sh:system/addon.d/50-cm.sh \
     vendor/to/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 
+# Backup Services whitelist
+PRODUCT_COPY_FILES += \
+    vendor/to/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
+
 # Signature compatibility validation
 PRODUCT_COPY_FILES += \
     vendor/to/prebuilt/common/bin/otasigcheck.sh:install/bin/otasigcheck.sh
