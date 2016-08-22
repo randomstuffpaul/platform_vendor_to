0fa784802733f8be9aeb684737c798c4371990ee
cjkacz <cjkacz@gmail.com>
2016-08-21 22:10:44 -0400

    D2: Go back to CM kernel
    Change-Id: Ie8c912e2f81be74a9009153a02ce41f3240eb2c9

--- product/samsung/d2-common/to.mk
+++ product/samsung/d2-common/to.mk
@@ -1,15 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-# Custom kernel defconfig (Must exist in kernel source)
-TARGET_KERNEL_CONFIG_CUSTOM := octos_d2_defconfig
-
-# Build using sabermod toolchain on linux but use default toolchain on darwin.
-ifneq ($(HOST_OS),darwin)
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9-uber/bin
-TARGET_GCC_VERSION_EXP := 5.3-uber
-endif
-
 # Team OctOs device maintainer info
 ifneq (COMMUNITY, $(TO_BUILDTYPE))
 DEVICE_PACKAGE_OVERLAYS += vendor/to/product/samsung/d2-common/maintainer
