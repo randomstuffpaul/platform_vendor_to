79952c8e8429977e36dddb98e62fc057b9ff77ec
hedwig34 <hedwig34@gmail.com>
2016-09-13 02:44:01 -0400

    Add back Gallery2, Camera2, and DeskClock
    Change-Id: I12959bbe7b32dbd480079f8f4f8413343f5f8064

--- config/common_full.mk
+++ config/common_full.mk
@@ -3,6 +3,9 @@ $(call inherit-product, vendor/to/config/common.mk)
 
 # Main Required Packages
 PRODUCT_PACKAGES += \
+    Camera2 \
+    DeskClock \
+    Gallery2 \
     Launcher3 \
     LiveWallpapersPicker
 
