4e1674286ee65190e88a6297cd26e9e822d8c559
hedwig34 <hedwig34@gmail.com>
2016-11-21 15:27:16 -0500

    Fix order in vendorsetup
    Change-Id: Ifb161d2a2f6d3020b0d840a75d2a131777c1b455

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 add_lunch_combo to_angler-userdebug
 add_lunch_combo to_bullhead-userdebug
+add_lunch_combo to_hammerhead-userdebug
 add_lunch_combo to_marlin-userdebug
 add_lunch_combo to_shamu-userdebug
-add_lunch_combo to_hammerhead-userdebug
