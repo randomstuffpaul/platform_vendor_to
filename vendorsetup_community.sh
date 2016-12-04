e0134d740d8785f1cbfa459573f694c059939045
ujwalp15 <ujwalp09@gmail.com>
2016-12-04 20:47:10 +0530

    oneplus3: bring up oneplus3 to 7.1
    Change-Id: I88e21cbe012dc22de4ea51a8fe9bef429358a12c

--- vendorsetup_community.sh
+++ vendorsetup_community.sh
@@ -2,3 +2,4 @@ add_lunch_combo to_armani-userdebug
 add_lunch_combo to_bacon-userdebug
 add_lunch_combo to_cancro-userdebug
 add_lunch_combo to_grouper-userdebug
+add_lunch_combo to_oneplus3-userdebug
