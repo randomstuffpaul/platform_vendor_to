aospcus/android_vendor_du

Add SuperSU [1/2]
Change-Id: Ib8646a04442abc8b509a004c3d3bb1616f0b588e

config/common.mk
@@ -104,6 +104,11 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/du/overlay/common
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
 
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/du/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
 # Versioning System
 ANDROID_VERSION = 5.0.1
 DU_VERSION = v9.0

randomstuffpaul/platform_vendor_to

Change-Id: Id5d3eb45b20ba4e3475682a0feb0fbcd1327c592

config/common.mk
@@ -90,6 +90,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/to/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml
 
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/to/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/to/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
 # Theme engine
 include vendor/to/config/themes_common.mk
 