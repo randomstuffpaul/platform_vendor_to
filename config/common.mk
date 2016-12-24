df0c6187c548330467e60b8380c504555fac194a
cjkacz <cjkacz@gmail.com>
2016-12-23 23:07:25 -0500

    Enable assistant on all devices
    Change-Id: I8ac212da4b71ced6ef1f1d060d7a0d6fb34ef004

--- config/common.mk
+++ config/common.mk
@@ -26,7 +26,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.build.selinux=1 \
-    ro.com.android.dataroaming=false
+    ro.com.android.dataroaming=false \
+    ro.opa.eligible_device=true
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
