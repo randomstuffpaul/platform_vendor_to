2de54b20d20b9d4b3c7e164208c4569bc48d72d9
John <manfromgta@gmail.com>
2017-01-03 17:53:02 -0500

    Team-OctOs build script with options
    Credits to pac-rom dev team
    
    modified for Oct N
    
    Use: Put octosify.sh in PATH, execute octosify.sh from root of working dir.
    
    Change-Id: I2339adb30698171b596324b293b22eeb1eddbf0c

---
+++ tools/octosify.sh
@@ -0,0 +1,252 @@
+#!/bin/bash
+#
+# octosify.sh: the overarching build script for the ROM.
+# Copyright (C) 2016-2017 Team-OctOs Project
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+usage() {
+    echo -e "${bldblu}Usage:${bldcya}"
+    echo -e "  octosify.sh [options] device"
+    echo ""
+    echo -e "${bldblu}  Options:${bldcya}"
+    echo -e "    -c# Cleaning options before build:"
+    echo -e "        1 - Run make clean"
+    echo -e "        2 - Run make installclean"
+    echo -e "    -d  Build rom without ccache"
+    echo -e "    -i  Ignore minor errors during build"
+    echo -e "    -j# Set number of jobs"
+    echo -e "    -k  Rewrite roomservice after dependencies update"
+    echo -e "    -r  Reset source tree before build"
+    echo -e "    -s# Sync options before build:"
+    echo -e "        1 - Normal sync"
+    echo -e "        2 - Make snapshot"
+    echo -e "        3 - Restore previous snapshot, then snapshot sync"
+    echo -e "    -w#  Log file options:"
+    echo -e "        1 - Send warnings and errors to a log file"
+    echo -e "        2 - Send all output to a log file"
+    echo ""
+    echo -e "${bldblu}  Example:${bldcya}"
+    echo -e "    ./octosify.sh -c1 shamu"
+    echo -e "${rst}"
+    exit 1
+}
+
+
+# Import Colors
+. ./vendor/to/tools/colors
+. ./vendor/to/tools/res/octos-start
+
+
+# Check directories
+if [ ! -d ".repo" ]; then
+    echo -e "${bldred}No .repo directory found. Is this an Android build tree?${rst}"
+    echo ""
+    exit 1
+fi
+if [ ! -d "vendor/to/" ]; then
+    echo -e "${bldred}No vendor/to directory found. Is this a OCTOS build tree?${rst}"
+    echo ""
+    exit 1
+fi
+
+
+opt_clean=0
+opt_ccache=0
+opt_jobs="$CPUS"
+opt_kr=0
+opt_ignore=0
+opt_reset=0
+opt_sync=0
+opt_log=0
+
+while getopts "c:d:gij:ko:rs:w:" opt; do
+    case "$opt" in
+    c) opt_clean="$OPTARG" ;;
+    d) opt_ccache=1 ;;
+    i) opt_ignore=1 ;;
+    j) opt_jobs="$OPTARG" ;;
+    k) opt_kr=1 ;;
+    r) opt_reset=1 ;;
+    s) opt_sync="$OPTARG" ;;
+    w) opt_log="$OPTARG" ;;
+    *) usage
+    esac
+done
+
+shift $((OPTIND-1))
+if [ "$#" -ne 1 ]; then
+    usage
+fi
+device="$1"
+
+
+# Ccache options
+if [ "$opt_ccache" -eq 1 ]; then
+    echo -e "${bldcya}Ccache not used in this build${rst}"
+    unset USE_CCACHE
+    echo ""
+fi
+
+
+# Check if last build was made ignoring errors
+# Set if unset
+if [ -f ".ignore_err" ]; then
+   : ${TARGET_IGNORE_ERRORS:=$(cat .ignore_err)}
+else
+   : ${TARGET_IGNORE_ERRORS:=false}
+fi
+
+export TARGET_IGNORE_ERRORS
+
+if [ "$TARGET_IGNORE_ERRORS" == "true" ]; then
+    opt_clean=1
+    echo -e "${bldred}Last build ignored errors. Cleaning Out${rst}"
+    echo ""
+    unset TARGET_IGNORE_ERRORS
+    echo -e "false" > .ignore_err
+fi
+
+
+# Cleaning out directory
+if [ "$opt_clean" -eq 1 ]; then
+    echo -e "${bldcya}Cleaning output directory${rst}"
+    make clean >/dev/null
+    echo -e "${bldcya}Output directory is: ${bldgrn}Clean${rst}"
+    echo ""
+elif [ "$opt_clean" -eq 2 ]; then
+    . build/envsetup.sh
+    lunch "to_$device-userdebug"
+    make installclean >/dev/null
+    echo -e "${bldcya}Output directory is: ${bldred}Dirty${rst}"
+    echo ""
+else
+    if [ -d "$OUTDIR/target" ]; then
+        echo -e "${bldcya}Output directory is: ${bldylw}Clean${rst}"
+        echo ""
+else
+        echo -e "${bldcya}Output directory is: ${bldgrn}Untouched${rst}"
+        echo ""
+    fi
+fi
+
+
+# Reset source tree
+if [ "$opt_reset" -ne 0 ]; then
+    echo -e "${bldcya}Resetting source tree and removing all uncommitted changes${rst}"
+    repo forall -c "git reset --hard HEAD; git clean -qf"
+    echo ""
+fi
+
+
+# Repo sync/snapshot
+if [ "$opt_sync" -eq 1 ]; then
+    # Sync with latest sources
+    echo -e "${bldcya}Fetching latest sources${rst}"
+    repo sync -qj"$opt_jobs"
+    echo ""
+elif [ "$opt_sync" -eq 2 ]; then
+    # Take snapshot of current sources
+    echo -e "${bldcya}Making a snapshot of the repo${rst}"
+    repo manifest -o snapshot-"$device".xml -r
+    echo ""
+elif [ "$opt_sync" -eq 3 ]; then
+    # Restore snapshot tree, then sync with latest sources
+    echo -e "${bldcya}Restoring last snapshot of sources${rst}"
+    echo ""
+    cp snapshot-"$device".xml .repo/manifests/
+
+    # Prevent duplicate projects
+    cd .repo/local_manifests || exit
+    for file in *.xml; do
+        mv "$file" "$(echo $file | sed 's/\(.*\.\)xml/\1xmlback/')"
+    done
+
+    # Start snapshot file
+    cd "$DIR" || exit
+    repo init -m snapshot-"$device".xml
+    echo -e "${bldcya}Fetching snapshot sources${rst}"
+    echo ""
+    repo sync -qdj"$opt_jobs"
+
+    # Prevent duplicate backups
+    cd .repo/local_manifests || exit
+    for file in *.xmlback; do
+        mv "$file" "$(echo $file | sed 's/\(.*\.\)xmlback/\1xml/')"
+    done
+
+    # Remove snapshot file
+    cd "$DIR" || exit
+    rm -f .repo/manifests/snapshot-"$device".xml
+    repo init
+fi
+
+
+# Setup environment
+echo -e "${bldcya}Setting up environment${rst}"
+echo -e "${bldmag}${line}${rst}"
+. build/envsetup.sh
+echo -e "${bldmag}${line}${rst}"
+
+
+# This will create a new build.prop with updated build time and date
+rm -f "$OUTDIR"/target/product/"$device"/system/build.prop
+
+# This will create a new .version for kernel version is maintained on one
+rm -f "$OUTDIR"/target/product/"$device"/obj/KERNEL_OBJ/.version
+
+
+# Lunch device
+echo ""
+echo -e "${bldcya}Lunching device${rst}"
+lunch "to_$device-userdebug"
+
+
+# Ignore minor errors during build
+if [ "$opt_ignore" -eq 1 ]; then
+    opt_i=" "-k
+    export TARGET_IGNORE_ERRORS=true
+    echo -e "true" > .ignore_err
+    if [ "$opt_log" -eq 0 ]; then
+        opt_log=1
+    fi
+else
+    opt_i=""
+fi
+
+
+# Log file options
+if [ "$opt_log" -ne 0 ]; then
+    rm -rf build.log
+    if [ "$opt_log" -eq 1 ]; then
+        exec 2> >(sed -r 's/'$(echo -e "\033")'\[[0-9]{1,2}(;([0-9]{1,2})?)?[mK]//g' | tee -a build.log)
+    else
+        exec &> >(tee -a build.log)
+    fi
+fi
+
+
+# Start compilation
+    echo -e "${bldcya}Starting compilation: ${bldgrn}Building ${bldylw}OctOs N $VO${rst}"
+    echo ""
+    make -j"$opt_jobs$opt_v$opt_i" bacon
+
+
+# Cleanup unused built
+rm -f "$OUTDIR"/target/product/"$device"/oct_*-ota*.zip
+
+
+echo ""
